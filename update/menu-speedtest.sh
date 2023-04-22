8064839dcc014da5140ad859b2b14ee78993ab0b
ZEAKING X DXT <129081220+Zeadxt@users.noreply.github.com>
2023-04-22 17:25:18 +0700

    Update menu-speedtest.sh

--- update/menu-speedtest.sh
+++ update/menu-speedtest.sh
@@ -18,7 +18,7 @@ export LIGHT='\033[0;37m'
 export NC='\033[0m'
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/JurigVPN/izinvps/ipuk/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/Zeadxt/kzl/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -36,7 +36,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/JurigVPN/izinvps/ipuk/ip | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/Zeadxt/kzl/main/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -53,7 +53,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/JurigVPN/izinvps/ipuk/ip | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Zeadxt/kzl/main/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -86,7 +86,7 @@ speedtest
 echo -e ""
 echo -e " $PURPLE└───────────────────────────────────────────────┘${NC}"
 echo -e "$PURPLE┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$PURPLE│${NC}                 • JurigVPN •                $PURPLE│$NC"
+echo -e "$PURPLE│${NC}                 • FREE TUNNEL •                $PURPLE│$NC"
 echo -e "$PURPLE└─────────────────────────────────────────────────┘${NC}"
 echo -e ""
 read -n 1 -s -r -p "  Press any key to back on menu"
