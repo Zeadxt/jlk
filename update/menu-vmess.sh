68f5e94f7596a35c255dfb0b97727afc78d3df15
ZEAKING X DXT <129081220+Zeadxt@users.noreply.github.com>
2023-04-23 05:54:15 +0700

    Update menu-vmess.sh

--- update/menu-vmess.sh
+++ update/menu-vmess.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+export izin="https://raw.githubusercontent.com/Zeadxt/kzl/main/ip"
+
 ###########- COLOR CODE -##############
 
 colornow=$(cat /etc/ssnvpn/theme/color.conf)
@@ -9,34 +11,64 @@ RED="\033[0;31m"
 COLOR1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
 COLBG1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
 ###########- END COLOR CODE -##########
-VALIDITY() {
-    today=$(date -d "0 days" +"%Y-%m-%d")
-    Exp1=$(curl https://raw.githubusercontent.com/Zeadxt/kzl/main/ip | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-        echo -e "\e[32mAUTOSCRIPT SUKSES..\e[0m"
-        sleep 5
+BURIQ () {
+    curl -sS ${izin} > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
     else
-    echo -e "\e[31mScript Anda Telah Expired !!\e[0m";
-    echo -e "\e[31mTolong Renew ke Owner Script \e[0m"
-    exit 0
+    rm -f /etc/.$user.ini > /dev/null 2>&1
     fi
+    done
+    rm -f /root/tmp
 }
-IZIN=$(curl https://raw.githubusercontent.com/Zeadxt/kzl/main/ip | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPERMISSION ACCEPT...\e[0m"
-    VALIDITY
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS ${izin} | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
 else
-    echo -e "\e[31mPermohonan Ditolak!\e[0m"
-    echo -e "${YELLOW}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    echo -e "                PERMISSION DENIED ! "
-    echo -e "     Your VPS ${NC}( ${green}$IP${NC} ) ${YELLOW}Has been Banned "
-    echo -e "         Buy access permissions for scripts "
-    echo -e "                 Contact Admin :"
-    echo -e "             ${green}Telegram t.me/@freetunnelproject "
-    echo -e "${YELLOW}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo ""
-    rm -f setup.sh
-    exit 0
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS ${izin} | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
 fi
 clear
 
