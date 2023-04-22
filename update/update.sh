e86255b5b1dfff3a396236e1c8da8b17a1afea24
ZEAKING X DXT <129081220+Zeadxt@users.noreply.github.com>
2023-04-22 17:42:21 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+export url = "https://raw.githubusercontent.com/Zeadxt/jlk/main"
 ###########- COLOR CODE -##############
 echo -e " [INFO] Downloading Update File"
 sleep 2
